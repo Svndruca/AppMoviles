47272bf52d439d29e4cf84ddf28f62e8c6444aaa
Svndruca <129185475+Svndruca@users.noreply.github.com>
2023-09-08 20:22:09 -0300

    prueba 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  isLoading: boolean = false;
+  currentPage: string = 'RegistrQR';
+  usuario: string = '';
 
-  constructor() {}
+  constructor(
+    private menuCtrl: MenuController,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.queryParams.subscribe((params) => {
+      if (params['usuario']) {
+        this.usuario = params['usuario'];
+      }
+    });
+  }
 
+  toggleMenu() {
+    this.menuCtrl.toggle();
+  }
+
+  redirectTo(page: string) {
+    this.menuCtrl.close();
+    this.router.navigate([page], { queryParams: { usuario: this.usuario } });
+    this.currentPage = page === 'estudiante' ? 'Estudiante' : 'Escanear QR';
+  }
+
+  redireccionarAHome() {
+    this.isLoading = true; 
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 9000); // segundos
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
